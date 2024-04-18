795280d6f59bf43c09ad11144effde27e53aac5c
Saiprasad.b <bowrojusaiprasad.com>
2024-04-18 18:33:21 +0530

    Sais commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/1/809d5fb5d5fc001e1001e1de8e202122
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kranthi
+ *
+ */
+module Model {
+}
---
+++ Model/src/com/model/Model.java
@@ -0,0 +1,11 @@
+package com.model;
+
+public class Model {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Jai Sree Ram");
+		
+	}
+
+}
